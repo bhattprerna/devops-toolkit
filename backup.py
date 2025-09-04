c77c5d0e9adf93cd7a538eb909a3080b8830ad3a
Prerna <bhattprerna673@gmail.com>
2025-09-04 18:45:38 +0000

    change in backup file

--- backup.py
+++ backup.py
@@ -2,7 +2,7 @@ import os
 import tarfile
 from datetime import datetime
 
-source_dir = "/home/user/data"
+source_dir = "/home/control/devops-toolkit"
 backup_file = f"backup_{datetime.now().strftime('%Y%m%d%H%M%S')}.tar.gz"
 
 with tarfile.open(backup_file, "w:gz") as tar:
