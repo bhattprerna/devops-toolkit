3314d323a2a8af9dc5c20372983932c87f699d20
Prerna <bhattprerna673@gmail.com>
2025-09-04 18:32:34 +0000

    Add backup.py file

---
+++ backup.py
@@ -0,0 +1,12 @@
+import os
+import tarfile
+from datetime import datetime
+
+source_dir = "/home/user/data"
+backup_file = f"backup_{datetime.now().strftime('%Y%m%d%H%M%S')}.tar.gz"
+
+with tarfile.open(backup_file, "w:gz") as tar:
+    tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+print(f"Backup created: {backup_file}")
+ 
